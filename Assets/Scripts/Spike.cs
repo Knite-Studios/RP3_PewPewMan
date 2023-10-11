1139f47d05c465625d2cb4fef56216a66ff464c2
yayuli <pengyufentf@outlook.com>
2023-10-11 14:42:20 -0600

    tile maps done
    code by mos approved

--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -1,11 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
+[RequireComponent(typeof(TilemapCollider2D),typeof(CompositeCollider2D))]
 public class Spike : MonoBehaviour
 {
+    private void Awake()
+    {
+        TilemapCollider2D b = GetComponent<TilemapCollider2D>(); 
+        b.usedByComposite = true;
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.bodyType = RigidbodyType2D.Kinematic;
+
+        CompositeCollider2D a = GetComponent<CompositeCollider2D>();
+        a.isTrigger = true;
+
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
