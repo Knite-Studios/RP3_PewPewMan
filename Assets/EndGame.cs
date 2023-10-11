9b229240536b6fcbcb096a28d4cf9f5b5e0f76ca
Perz <s.ali224@mybvc.ca>
2023-10-10 23:10:21 -0600

    Final Polish Part 1

---
+++ Assets/EndGame.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class EndGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Application.Quit();
+            EditorApplication.isPlaying = false;
+        }
+    }
+}
