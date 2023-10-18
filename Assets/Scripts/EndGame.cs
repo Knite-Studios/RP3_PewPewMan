95a7f12ee8d0ccf3f7fec6ced3f7089ca47bb52e
Perz <s.ali224@mybvc.ca>
2023-10-17 21:23:37 -0600

    DIE

--- Assets/Scripts/EndGame.cs
+++ Assets/Scripts/EndGame.cs
@@ -1,33 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
-
+using UnityEngine.SceneManagement; 
 public class EndGame : MonoBehaviour
 {
     [SerializeField] PlayerData playerData;
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
-    // Update is called once per frame
+    private string mainMenuSceneName = "MainMenu";
+
     void Update()
     {
-      if(playerData.player.health <= 0)
+        if(playerData.player.health <= 0)
         {
-        Application.Quit();
-        EditorApplication.isPlaying = false;
-      }
+            LoadMainMenu();
+        }
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            Application.Quit();
-            EditorApplication.isPlaying = false;
+            LoadMainMenu();
         }
+    }
+
+    private void LoadMainMenu()
+    {
+        SceneManager.LoadScene(mainMenuSceneName);
     }
 }
