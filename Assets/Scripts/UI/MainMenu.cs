5268d121947e3072d489863b51b0065d5c80833a
Perz <s.ali224@mybvc.ca>
2023-10-17 18:35:47 -0600

    Loading

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MainMenu : Singleton<MainMenu>
@@ -9,26 +7,26 @@ public class MainMenu : Singleton<MainMenu>
     public GameObject warningMenu;
     public float waitTime = 5f;
     float timer = 0;
-    // Start is called before the first frame update
+
+    private int loadingSceneIndex = 1;
+
     void Start()
     {
         mainMenu.SetActive(false);
         warningMenu.SetActive(true);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
         timer += Time.deltaTime;
         if(timer > waitTime)
         {
             mainMenu.SetActive(true);
             warningMenu.SetActive(false);
         }
-     if(Input.GetKeyDown(KeyCode.J) && timer > waitTime)
+        if(Input.GetKeyDown(KeyCode.J) && timer > waitTime)
         {
-            UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+            UnityEngine.SceneManagement.SceneManager.LoadScene(loadingSceneIndex);
         }
     }
 }
